084463afa21bb0130ea8f64616690bdbba2f6faf
Nathan Margaglio <nathanmargaglio@gmail.com>
2021-05-23 18:59:18 +0000

    fix: update local provider version to 2.1

--- versions.tf
+++ versions.tf
@@ -16,7 +16,7 @@ terraform {
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.0"
+      version = "~> 2.1"
     }
     template = {
       source  = "hashicorp/template"
